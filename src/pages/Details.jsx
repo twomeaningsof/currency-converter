f0dfc0b7334041fc410833aac57dc7e620abffe8
Igor <twomeaningsof@gmail.com>
2022-05-12 11:11:40 +0200

    feat(app) implements fetch currencies rates on select value change

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { routes } from "../constants/routes";
 import Breadcrumbs from "../components/Breadcrumbs";
 import Heading from "../components/Heading";
 import Date from "../components/Date";
 import DetailsPageContent from "../components/DetailContent";
+import { useFetchTwoCurrenciesRates } from "../hooks/useFetchTwoCurrenciesRates";
 
 function DetailsPage({ currencies }) {
   const { baseCurrency, secondCurrency } = useParams();
+  const [currentSetUp, setCurrentSetUp] = useState({
+    base: baseCurrency,
+    second: secondCurrency,
+    change: 0,
+    reversedChange: 0,
+  });
+
+  const { baseToSecondCurrencyRate, secondToBaseCurrencyRate, loading, error } =
+    useFetchTwoCurrenciesRates(currentSetUp.base, currentSetUp.second);
+
+  currentSetUp.change = baseToSecondCurrencyRate;
+  currentSetUp.reversedChange = secondToBaseCurrencyRate;
 
   return (
     <>
@@ -14,11 +28,15 @@ function DetailsPage({ currencies }) {
       <Heading variant="h1">Currency converter</Heading>
       <Heading variant="h2">Convert values</Heading>
       <Date date="2022-04-25" format="LL" />
-      <DetailsPageContent
-        currencies={currencies}
-        baseCurrency={baseCurrency}
-        secondCurrency={secondCurrency}
-      />
+      {error && <div>{error.message}</div>}
+      {loading && <div>loading...</div>}
+      {!(error || loading) ? (
+        <DetailsPageContent
+          currencies={currencies}
+          currentSetUp={currentSetUp}
+          handleSelect={setCurrentSetUp}
+        />
+      ) : null}
     </>
   );
 }
