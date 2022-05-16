dc4662a2f02a3ff61a48b6d0e22733d7083c7d02
unknown <twomeaningsof@gmail.com>
2022-05-16 20:31:15 +0200

    fix(app): initial height js func

--- src/index.js
+++ src/index.js
@@ -9,3 +9,18 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+window.onresize(() => {
+  document.body.style.setProperty("overflow", "auto");
+  const metaViewport = document.querySelector("meta[name=viewport]");
+  metaViewport.setAttribute(
+    "content",
+    "height=" + initialHeight + "px, width=device-width, initial-scale=1.0"
+  );
+});
+
+// const metaViewport = document.querySelector("meta[name=viewport]");
+// metaViewport.setAttribute(
+//   "content",
+//   "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'"
+// );
